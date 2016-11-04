61e3297cf99ff9a7c2490001a6216ebd9ffe822a
Jared Blatt <jmblatt1@gmail.com>
2016-11-03 23:34:54 -0400

    Starts TaskListResource

--- jersey-backend/src/main/java/tasklist/rest/TaskListResource.java
+++ jersey-backend/src/main/java/tasklist/rest/TaskListResource.java
@@ -1,12 +1,37 @@
 package tasklist.rest;
 
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import tasklist.model.TaskList;
 
 @Path("/lists")
 public class TaskListResource {
 
-    public Response createTaskList(){
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createTaskList(TaskList taskList) {
+        return null;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllLists() {
+        return null;
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getList(@PathParam("id") String id) {
+        return null;
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateList(@PathParam("id") String id, TaskList tasklist) {
         return null;
     }
 
