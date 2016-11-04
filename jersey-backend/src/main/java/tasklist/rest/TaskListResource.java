cbb7f9b0adc7f4a488f810f256a4a519af42ca17
Jared Blatt <jmblatt1@gmail.com>
2016-11-03 22:20:50 -0400

    adds jersey, scaffold

---
+++ jersey-backend/src/main/java/tasklist/rest/TaskListResource.java
@@ -0,0 +1,13 @@
+package tasklist.rest;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/lists")
+public class TaskListResource {
+
+    public Response createTaskList(){
+        return null;
+    }
+
+}
